f5675d8b60c2a12e9e1120a7df4aa5629fcc4845
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-10-13 07:10:32 +0530

    updating files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('mongodb+srv://badgujardevesh24:oMJge23uDspR50tA@petpuja.2ubks.mongodb.net/petpuja').then(()=>console.log("DB Connected"));
-   
-}
-
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://badgujardevesh24:oMJge23uDspR50tA@petpuja.2ubks.mongodb.net/petpuja"
+    )
+    .then(() => console.log("DB Connected"));
+};
